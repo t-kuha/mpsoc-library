f96508a07bcbf98ba232f745bc2e8099617173d3
t-kuha <imagingtechnerd@gmail.com>
2018-10-29 05:20:40 +0000

    Add snappy (shared lib)

--- dl-framework/caffe-dependency/lib/cmake/Snappy/SnappyTargets-noconfig.cmake
+++ dl-framework/caffe-dependency/lib/cmake/Snappy/SnappyTargets-noconfig.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "Snappy::snappy" for configuration ""
 set_property(TARGET Snappy::snappy APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
 set_target_properties(Snappy::snappy PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
-  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libsnappy.a"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libsnappy.so.1.1.7"
+  IMPORTED_SONAME_NOCONFIG "libsnappy.so.1"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS Snappy::snappy )
-list(APPEND _IMPORT_CHECK_FILES_FOR_Snappy::snappy "${_IMPORT_PREFIX}/lib/libsnappy.a" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_Snappy::snappy "${_IMPORT_PREFIX}/lib/libsnappy.so.1.1.7" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
