828538011bb17d6ac9cc90f1085f0f7b048ef4d0
t-kuha <imagingtechnerd@gmail.com>
2018-10-29 09:17:47 +0000

    Add OpenBLAS

---
+++ dl-framework/caffe-dependency/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.3")
+SET(OpenBLAS_INCLUDE_DIRS /home/imagingtechnerd/work/OpenBLAS-0.3.3/_mpsoc/include)
+SET(OpenBLAS_LIBRARIES /home/imagingtechnerd/work/OpenBLAS-0.3.3/_mpsoc/lib/libopenblas.so)
