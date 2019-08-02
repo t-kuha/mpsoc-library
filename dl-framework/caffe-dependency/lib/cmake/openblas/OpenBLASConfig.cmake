e89b8767ea88bd94e61336e1057cecadf1413c48
t-kuha <imagingtechnerd@gmail.com>
2019-08-02 00:51:12 +0000

    Update OpenBLAS (for compatibility with libtorch)

---
+++ dl-framework/caffe-dependency/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.4")
+SET(OpenBLAS_INCLUDE_DIRS /home/imagingtechnerd/disk/blas/OpenBLAS-0.3.4/_test2/include)
+SET(OpenBLAS_LIBRARIES /home/imagingtechnerd/disk/blas/OpenBLAS-0.3.4/_test2/lib/libopenblas.so)
